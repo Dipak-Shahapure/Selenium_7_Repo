d1d6e18274acccdfa955d9a8f068aacd29169e9a
Lenovo <Lenovo@DESKTOP-QVQUTI8>
2025-05-16 08:56:48 +0530

    2nd commit

--- Automation_Framework_7/src/test/java/books/TC_DWS_001_Test.java
+++ Automation_Framework_7/src/test/java/books/TC_DWS_001_Test.java
@@ -20,6 +20,7 @@ public class TC_DWS_001_Test extends BaseClass {
 		Assert.assertEquals(driver.getTitle(), "Demo Web Shop. Books","Books Page is Not Dispalyed");
 		test.log(Status.PASS, "Books Page is Displayed");
 		test.log(Status.PASS, "Books Page is Displayed");
+		test.log(Status.PASS, "Books Page is Displayed");
 		
 	}
 
